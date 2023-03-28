d31f80e97f76a760a9b07d85b06db3dc754521f8
Elena Bogdanova <denissova@bk.ru>
2023-03-28 14:38:42 +0300

    added solution to the task

--- result_work/Program.cs
+++ result_work/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задание
+// Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+// длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. 
+// При решение не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+string[] array = new string[5] { "888", "18", "test", "work", "5final" };
+string[] arr = new string[array.Length];
+
+void ResultArray(string[] array, string[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            arr[count] = array[i];
+            count++;
+        }
+    }
+
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.WriteLine($" {array[i]} ");
+    }
+    System.Console.WriteLine();
+}
+
+ResultArray(array, arr);
+PrintArray(arr);
